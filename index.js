99988eb8239d1c0ec629c9c51861d14b25bd8232
jadhavshrutika@122.com <shrutikajad432@gmail.com>
2025-11-20 20:14:35 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,8 @@
+
+console.log('Starting test...');
+
+setTimeout(()=>{
+    console.log('Waiting # seconds....');
+},3000)
+
+console.log('Tests Complete.....');
---
+++ test.js
@@ -0,0 +1,4 @@
+
+console.log('Hello DeveOps');
+console.log('I`m learning CI/CD using Github Action');
+
